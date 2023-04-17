eecff4debb476723f9aeebabffe69d6ed411cae5
EtienneSicara <119853410+EtienneSicara@users.noreply.github.com>
2023-04-17 19:32:29 +0200

    add macros

--- macros/concat.sql
+++ macros/concat.sql
@@ -1,3 +1,3 @@
-{% macro concat(column_name, list_of_columns_to_concat, column_alias`) %}
-    {{ column_name }}::text {%- for col in list_of_columns_to_concat %} ({{ col }}) {%- if not loop.last -%} || {%- endif -%} {%- endfor %} as {{ column_alias }}
+{% macro concat(list_of_columns_to_concat, column_alias`) %}
+    {%- for col in list_of_columns_to_concat %} ({{ col }}::text) {%- if not loop.last -%} || {%- endif -%} {%- endfor %} as {{ column_alias }}
 {% endmacro %}
